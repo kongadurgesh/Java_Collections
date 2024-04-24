f0ff91fd95d5729e7ed7fcdaa90f4c2d01cec0c1
Durgesh Konga <kongadurgesh20@gmail.com>
2024-04-24 12:34:24 +0530

    added vector basic

---
+++ src/VectorDemo.java
@@ -0,0 +1,8 @@
+import java.util.Vector;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+        Vector<Integer> vector = new Vector<Integer>(1, 5);
+        System.out.println(vector);
+    }
+}

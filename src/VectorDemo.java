ff297a9edbbb911f7a274dcf5b70a4c3de868ce2
Durgesh Konga <kongadurgesh20@gmail.com>
2024-05-30 11:06:47 +0530

    initial commit for collections

--- src/VectorDemo.java
+++ src/VectorDemo.java
@@ -1,8 +1,18 @@
-import java.util.Vector;
-
 public class VectorDemo {
-    public static void main(String[] args) {
-        Vector<Integer> vector = new Vector<Integer>(1, 5);
-        System.out.println(vector);
+
+    public static void someMethod() {
+        String string1 = "Hi";
+        String string2 = string1;
+        System.out.println("string1: hashcode: " + string1.hashCode());
+        System.out.println("string2 hashCode: " + string2.hashCode());
+
+    }
+
+    class Student {
+        public Student(int id) {
+        }
+    }
+
+    public void main(String[] args) {
     }
 }

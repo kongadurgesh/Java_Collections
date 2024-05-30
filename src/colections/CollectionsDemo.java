441ff1f8e2d56c8dcd6cdb27aad657eb5937037c
Durgesh Konga <kongadurgesh20@gmail.com>
2024-05-30 11:58:26 +0530

    commit for collectiosn practise

---
+++ src/colections/CollectionsDemo.java
@@ -0,0 +1,150 @@
+package colections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CollectionsDemo {
+    public static void main(String[] args) {
+        listOperations();
+        setOperations();
+        mapOperations();
+        queueOperations();
+        sortList();
+        customObjectsList();
+        groupListsByProperty();
+        filterAndMapList();
+    }
+
+    // Filtering and Mapping
+    // Given a list of Person objects (similar to the previous questions), filter
+    // the list to include only persons with an age greater than or equal to 30.
+    // Map the filtered list to a list of strings containing only the names of the
+    // filtered persons.
+    private static void filterAndMapList() {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Alice", 30));
+        people.add(new Person("Bob", 25));
+        people.add(new Person("Charlie", 35));
+        people.add(new Person("Dan", 30));
+        people.add(new Person("Erik", 25));
+        people.add(new Person("Flit", 35));
+
+        System.out.println("Filtered Persons");
+        System.out.println(people.stream().filter(person -> person.getAge() > 30)
+                .map(Person::getName).toList());
+    }
+
+    // Group Elements by Property
+    // Given a list of Person objects (similar to the previous question), group the
+    // persons by their age.
+    private static void groupListsByProperty() {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Alice", 30));
+        people.add(new Person("Bob", 25));
+        people.add(new Person("Charlie", 35));
+        people.add(new Person("Dan", 30));
+        people.add(new Person("Erik", 25));
+        people.add(new Person("Flit", 35));
+
+        System.out.println("Persons grouped by age");
+        System.out.println(people.stream().collect(
+                Collectors.groupingBy(Person::getAge, Collectors.mapping(Person::getName, Collectors.toList()))));
+    }
+
+    private static void customObjectsList() {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Alice", 30));
+        people.add(new Person("Bob", 25));
+        people.add(new Person("Charlie", 35));
+
+        System.out.println("Sorted people based on age ascending");
+
+        System.out.println(people.stream().sorted((x, y) -> x.age - y.age).toList()
+                .stream().map(Person::getName).toList());
+    }
+
+    // Intermediate Level
+    // Sort a List
+    // Given a list of strings, sort the list in alphabetical order and in reverse
+    // alphabetical order.
+    private static void sortList() {
+        List<String> list = Arrays.asList("banana", "apple", "cherry");
+        System.out.println("alphabetical order");
+        System.out.println(list.stream().sorted().toList());
+        System.out.println("reverse alphabetical order");
+        System.out.println(list.stream().sorted().toList().reversed());
+    }
+
+    private static void queueOperations() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        System.out.println(queue);
+        queue.remove();
+        System.out.println("First Element:" + queue.peek());
+        for (Integer item : queue) {
+            System.out.println(item);
+        }
+    }
+
+    private static void mapOperations() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Alice", 30);
+        map.put("Bob", 25);
+        map.put("Charlie", 35);
+        System.out.println("Alice:" + map.get("Alice"));
+        map.remove("Bob");
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+    }
+
+    private static void setOperations() {
+        Set<String> set = new HashSet<>(Arrays.asList("apple", "banana", "cherry"));
+        set.add("date");
+        if (set.contains("banana"))
+            System.out.println("set contains banana");
+        set.remove("apple");
+        for (String item : set) {
+            System.out.println(item);
+        }
+    }
+
+    private static void listOperations() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
+        list.add(60);
+        System.out.println(list.get(2));
+        list.remove(1);
+        for (Integer item : list) {
+            System.out.println(item);
+        }
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
